e3dd85215cbf2979b98508ed04db92f63643c82a
Aaronthegreat-star <aaronhood38@gmail.com>
2024-06-24 22:39:26 +0100

    latest commit

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://fv0ui45wyg.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9xlcz1u006.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
